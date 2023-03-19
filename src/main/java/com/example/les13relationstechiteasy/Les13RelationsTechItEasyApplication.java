c2b5f38fd16fe2270804f59d3a64aae3785cb692
jaybk051183 <kailashbhaggoe@outlook.com>
2023-03-19 16:32:11 +0100

    project setup

---
+++ src/main/java/com/example/les13relationstechiteasy/Les13RelationsTechItEasyApplication.java
@@ -0,0 +1,13 @@
+package com.example.les13relationstechiteasy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Les13RelationsTechItEasyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Les13RelationsTechItEasyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/les13relationstechiteasy/Les13RelationsTechItEasyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.les13relationstechiteasy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Les13RelationsTechItEasyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
